e1fdbfd33d6090c7dfa196ed6c2eb719ba499348
MALLIKA JAIN <mallikaj@usc.edu>
2019-10-27 01:23:32 -0700

    click on bottom nav bar + profile page

--- app/src/main/java/com/example/beanandleaf/BottomNavigation.java
+++ app/src/main/java/com/example/beanandleaf/BottomNavigation.java
@@ -4,27 +4,89 @@ import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.fragment.app.Fragment;
+import android.view.MenuItem;
+//import androidx.navigation.NavController;
+//import androidx.navigation.Navigation;
+//import androidx.navigation.ui.AppBarConfiguration;
+//import androidx.navigation.ui.NavigationUI;
 
-public class BottomNavigation extends AppCompatActivity {
+//public class BottomNavigation extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+////        super.onCreate(savedInstanceState);
+////        setContentView(R.layout.activity_bottom_navigation);
+////        BottomNavigationView navView = findViewById(R.id.nav_view);
+////        // Passing each menu ID as a set of Ids because each
+////        // menu should be considered as top level destinations.
+////        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+////                R.id.navigation_map, R.id.navigation_profile, R.id.navigation_history, R.id.navigation_recommendations)
+////                .build();
+////        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+////        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+////        NavigationUI.setupWithNavController(navView, navController);
+//
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_bottom_navigation);
+//    }
+//
+//}
+
+//implement the interface OnNavigationItemSelectedListener in your activity class
+public class BottomNavigation extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navigation);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+
+        //loading the default fragment
+        loadFragment(new MapFragment());
+
+        //getting bottom navigation view and attaching the listener
+        BottomNavigationView navigation = findViewById(R.id.nav_view);
+        navigation.setOnNavigationItemSelectedListener(this);
     }
 
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.navigation_map:
+                fragment = new MapFragment();
+                break;
+
+            case R.id.navigation_profile:
+                fragment = new ProfileFragment();
+                break;
+
+            case R.id.navigation_history:
+                fragment = new HistoryFragment();
+                break;
+
+            case R.id.navigation_recommendations:
+                fragment = new RecommendationsFragment();
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
 }
