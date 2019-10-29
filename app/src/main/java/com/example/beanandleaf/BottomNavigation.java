3a0363db3bf65d8f21b3b1dbe4059c99557a54b1
MALLIKA JAIN <mallikaj@usc.edu>
2019-10-29 15:36:21 -0700

    added store fragment for merchant

--- app/src/main/java/com/example/beanandleaf/BottomNavigation.java
+++ app/src/main/java/com/example/beanandleaf/BottomNavigation.java
@@ -8,32 +8,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import android.view.MenuItem;
-//import androidx.navigation.NavController;
-//import androidx.navigation.Navigation;
-//import androidx.navigation.ui.AppBarConfiguration;
-//import androidx.navigation.ui.NavigationUI;
-
-//public class BottomNavigation extends AppCompatActivity {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-////        super.onCreate(savedInstanceState);
-////        setContentView(R.layout.activity_bottom_navigation);
-////        BottomNavigationView navView = findViewById(R.id.nav_view);
-////        // Passing each menu ID as a set of Ids because each
-////        // menu should be considered as top level destinations.
-////        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-////                R.id.navigation_map, R.id.navigation_profile, R.id.navigation_history, R.id.navigation_recommendations)
-////                .build();
-////        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-////        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-////        NavigationUI.setupWithNavController(navView, navController);
-//
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_bottom_navigation);
-//    }
-//
-//}
 
 //implement the interface OnNavigationItemSelectedListener in your activity class
 public class BottomNavigation extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
