ca8acd81b27ae7b7648e94f10bfc4cd77cd5d7b5
Ethan Smith <ethanasm@usc.edu>
2019-11-01 20:19:16 -0700

    Store dropdown on store page pulls from database

--- app/src/main/java/com/example/beanandleaf/StoreFragment.java
+++ app/src/main/java/com/example/beanandleaf/StoreFragment.java
@@ -1,15 +1,22 @@
 package com.example.beanandleaf;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+
+import java.util.ArrayList;
+
+import database.DatabaseHelper;
 
 public class StoreFragment extends Fragment {
     @Nullable
@@ -24,6 +31,16 @@ public class StoreFragment extends Fragment {
 
     public void onViewCreated(View view, Bundle savedInstanceState) {
         final Button addStoreButton = view.findViewById(R.id.add_store);
+
+        SharedPreferences pref = getActivity().getApplicationContext().getSharedPreferences("MyPref", 0);
+        final String email = pref.getString("email", null);
+        final String userType = pref.getString("userType", null);
+        DatabaseHelper db = new DatabaseHelper(getActivity());
+        ArrayList<String> spinnerArray = db.getStores(db.getUserId(email, userType));
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, spinnerArray);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner sItems = view.findViewById(R.id.store_spinner);
+        sItems.setAdapter(adapter);
 
         addStoreButton.setOnClickListener(new View.OnClickListener() {
             @Override
