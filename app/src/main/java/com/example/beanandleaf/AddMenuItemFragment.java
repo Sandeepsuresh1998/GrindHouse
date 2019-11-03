5a378f49214fb2afb2ae14eef380ab5d3637105c
Ethan Smith <ethanasm@usc.edu>
2019-11-03 00:13:58 -0700

    Fixed add menu item button

--- app/src/main/java/com/example/beanandleaf/AddMenuItemFragment.java
+++ app/src/main/java/com/example/beanandleaf/AddMenuItemFragment.java
@@ -27,7 +27,9 @@ public class AddMenuItemFragment extends Fragment {
 
     public void onViewCreated(View view, Bundle savedInstanceState) {
         final TextView nameEdit = view.findViewById(R.id.item_name_edit);
-        final TextView caloriesEdit = view.findViewById(R.id.calories_edit);
+        final TextView calSmallEdit = view.findViewById(R.id.calories_for_small_edit);
+        final TextView calMediumEdit = view.findViewById(R.id.calories_for_medium_edit);
+        final TextView calLargeEdit = view.findViewById(R.id.calories_for_large_edit);
         final TextView cafSmallEdit = view.findViewById(R.id.caffeine_small_edit);
         final TextView cafMediumEdit = view.findViewById(R.id.caffeine_medium_edit);
         final TextView cafLargeEdit = view.findViewById(R.id.caffeine_large_edit);
@@ -40,7 +42,9 @@ public class AddMenuItemFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 String name = nameEdit.getText().toString();
-                String calories = caloriesEdit.getText().toString();
+                String calSmall = calSmallEdit.getText().toString();
+                String calMedium = calMediumEdit.getText().toString();
+                String calLarge = calLargeEdit.getText().toString();
                 String cafSmall = cafSmallEdit.getText().toString();
                 String cafMedium = cafMediumEdit.getText().toString();
                 String cafLarge = cafLargeEdit.getText().toString();
@@ -50,66 +54,83 @@ public class AddMenuItemFragment extends Fragment {
                 boolean addSmall = false;
                 boolean addMedium = false;
                 boolean addLarge = false;
-                if (name.contentEquals("") || calories.contentEquals(""))
+                if (name.contentEquals(""))
                     return;
 
-                if (!isValidInteger(calories)) {
-                    Toast.makeText(getActivity().getApplicationContext(), "Please enter calories as a valid integer", Toast.LENGTH_LONG).show();
-                    return;
-                }
 
-                if (cafSmall.contentEquals("") && !priceSmall.contentEquals("")) {
+                if (cafSmall.contentEquals("") && !priceSmall.contentEquals("") && !calSmall.contentEquals("")) {
                     Toast.makeText(getActivity().getApplicationContext(), "Please enter the amount of caffeine for the small size", Toast.LENGTH_LONG).show();
                     return;
                 }
-                if (!cafSmall.contentEquals("") && priceSmall.contentEquals("")) {
+                if (!cafSmall.contentEquals("") && priceSmall.contentEquals("") && !calSmall.contentEquals("")) {
                     Toast.makeText(getActivity().getApplicationContext(), "Please enter a price for the small size", Toast.LENGTH_LONG).show();
                     return;
                 }
-                if (!cafSmall.contentEquals("") && !priceSmall.contentEquals("")) {
+                if (!cafSmall.contentEquals("") && !priceSmall.contentEquals("") && calSmall.contentEquals("")) {
+                    Toast.makeText(getActivity().getApplicationContext(), "Please enter the number of calories for the small size", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (!cafSmall.contentEquals("") && !priceSmall.contentEquals("") && !calSmall.contentEquals("")) {
                     if (!isValidInteger(cafSmall)) {
                         Toast.makeText(getActivity().getApplicationContext(), "Please enter a valid caffeine amount for the small size", Toast.LENGTH_LONG).show();
                     }
                     else if (!isValidDouble(priceSmall)) {
                         Toast.makeText(getActivity().getApplicationContext(), "Please enter a valid price for the small size", Toast.LENGTH_LONG).show();
                     }
+                    else if (!isValidInteger(calSmall)) {
+                        Toast.makeText(getActivity().getApplicationContext(), "Please enter a valid number of calories for the small size", Toast.LENGTH_LONG).show();
+                    }
                     else {
                         addSmall = true;
                     }
                 }
-                if (cafMedium.contentEquals("") && !priceMedium.contentEquals("")) {
+                if (cafMedium.contentEquals("") && !priceMedium.contentEquals("") && !calMedium.contentEquals("")) {
                     Toast.makeText(getActivity().getApplicationContext(), "Please enter the amount of caffeine for the medium size", Toast.LENGTH_LONG).show();
                     return;
                 }
-                if (!cafMedium.contentEquals("") && priceMedium.contentEquals("")) {
+                if (!cafMedium.contentEquals("") && priceMedium.contentEquals("") && !calMedium.contentEquals("")) {
                     Toast.makeText(getActivity().getApplicationContext(), "Please enter a price for the medium size", Toast.LENGTH_LONG).show();
                     return;
                 }
-                if (!cafMedium.contentEquals("") && !priceMedium.contentEquals("")) {
+                if (!cafMedium.contentEquals("") && !priceMedium.contentEquals("") && calMedium.contentEquals("")) {
+                    Toast.makeText(getActivity().getApplicationContext(), "Please enter the number of calories for the medium size", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (!cafMedium.contentEquals("") && !priceMedium.contentEquals("") && !calMedium.contentEquals("")) {
                     if (!isValidInteger(cafMedium)) {
                         Toast.makeText(getActivity().getApplicationContext(), "Please enter a valid caffeine amount for the medium size", Toast.LENGTH_LONG).show();
                     }
                     else if (!isValidDouble(priceMedium)) {
                         Toast.makeText(getActivity().getApplicationContext(), "Please enter a valid price for the medium size", Toast.LENGTH_LONG).show();
                     }
+                    else if (!isValidInteger(calMedium)) {
+                        Toast.makeText(getActivity().getApplicationContext(), "Please enter a valid number of calories for the medium size", Toast.LENGTH_LONG).show();
+                    }
                     else {
                         addMedium = true;
                     }
                 }
-                if (cafLarge.contentEquals("") && !priceLarge.contentEquals("")) {
+                if (cafLarge.contentEquals("") && !priceLarge.contentEquals("") && !calLarge.contentEquals("")) {
                     Toast.makeText(getActivity().getApplicationContext(), "Please enter the amount of caffeine for the large size", Toast.LENGTH_LONG).show();
                     return;
                 }
-                if (!cafLarge.contentEquals("") && priceLarge.contentEquals("")) {
+                if (!cafLarge.contentEquals("") && priceLarge.contentEquals("") && !calLarge.contentEquals("")) {
                     Toast.makeText(getActivity().getApplicationContext(), "Please enter a price for the large size", Toast.LENGTH_LONG).show();
                     return;
                 }
-                if (!cafLarge.contentEquals("") && !priceLarge.contentEquals("")) {
+                if (!cafLarge.contentEquals("") && !priceLarge.contentEquals("") && calLarge.contentEquals("")) {
+                    Toast.makeText(getActivity().getApplicationContext(), "Please enter the number of calories for the large size", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (!cafLarge.contentEquals("") && !priceLarge.contentEquals("") && !calLarge.contentEquals("")) {
                     if (!isValidInteger(cafLarge)) {
                         Toast.makeText(getActivity().getApplicationContext(), "Please enter a valid caffeine amount for the large size", Toast.LENGTH_LONG).show();
                     }
                     else if (!isValidDouble(priceLarge)) {
                         Toast.makeText(getActivity().getApplicationContext(), "Please enter a valid price for the large size", Toast.LENGTH_LONG).show();
+                    }
+                    else if (!isValidInteger(calLarge)) {
+                        Toast.makeText(getActivity().getApplicationContext(), "Please enter a valid number of calories for the large size", Toast.LENGTH_LONG).show();
                     }
                     else {
                         addLarge = true;
@@ -121,19 +142,19 @@ public class AddMenuItemFragment extends Fragment {
                 int storeID = pref.getInt("selectedStore", 0);
 
                 if (addSmall) {
-                    if (!db.insertMenuItem(storeID, name, calories, "Small", cafSmall, priceSmall)) {
+                    if (!db.insertMenuItem(storeID, name, calSmall, "Small", cafSmall, priceSmall)) {
                         Toast.makeText(getActivity().getApplicationContext(), "Error: small menu item not added", Toast.LENGTH_SHORT).show();
                         return;
                     }
                 }
                 if (addMedium) {
-                    if (!db.insertMenuItem(storeID, name, calories, "Medium", cafMedium, priceMedium)) {
+                    if (!db.insertMenuItem(storeID, name, calMedium, "Medium", cafMedium, priceMedium)) {
                         Toast.makeText(getActivity().getApplicationContext(), "Error: medium menu item not added", Toast.LENGTH_SHORT).show();
                         return;
                     }
                 }
                 if (addLarge) {
-                    if (!db.insertMenuItem(storeID, name, calories, "Large", cafLarge, priceLarge)) {
+                    if (!db.insertMenuItem(storeID, name, calLarge, "Large", cafLarge, priceLarge)) {
                         Toast.makeText(getActivity().getApplicationContext(), "Error: large menu item not added", Toast.LENGTH_SHORT).show();
                         return;
                     }
