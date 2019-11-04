b9760b21d8169b1a3fa387436f55a3be769744b8
Shayanna Ahuja <shayanna@usc.edu>
2019-11-04 10:36:45 -0800

    added changePassword Activity

---
+++ app/src/main/java/com/example/beanandleaf/ChangePassword.java
@@ -0,0 +1,17 @@
+package com.example.beanandleaf;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChangePassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+
+    }}
+
+
+
