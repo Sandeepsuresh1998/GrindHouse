bc61bb1c59c2c75676ffbe41fd685738977a00f5
Shayanna Ahuja <shayanna@usc.edu>
2019-11-21 22:39:51 -0800

    added recTest file

--- app/src/androidTest/java/com/example/beanandleaf/RecommendationsTest.java
+++ app/src/androidTest/java/com/example/beanandleaf/RecommendationsTest.java
@@ -5,18 +5,9 @@ import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import static org.junit.Assert.*;
 
-import static org.junit.Assert.assertEquals;
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-@RunWith(JUnit4.class)
-public class ExampleInstrumentedTest {
+public class RecommendationsTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
