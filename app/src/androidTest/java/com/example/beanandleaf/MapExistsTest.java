ba7a05a5560e238b2a63304c5c93bcd0eafac872
Ethan Smith <ethanasm@usc.edu>
2019-11-24 10:32:08 -0800

    Added my two black box test files

--- app/src/androidTest/java/com/example/beanandleaf/MapExistsTest.java
+++ app/src/androidTest/java/com/example/beanandleaf/MapExistsTest.java
@@ -35,6 +35,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 
+// Ethan will finish this
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class MapExistsTest {
--- app/src/androidTest/java/com/example/beanandleaf/MerchantVerifyTest.java
+++ app/src/androidTest/java/com/example/beanandleaf/MerchantVerifyTest.java
@@ -4,6 +4,7 @@ import androidx.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
 
+// Ethan will finish this
 public class MerchantVerifyTest {
     @Rule
     public ActivityTestRule<LandingPage> mActivityTestRule = new ActivityTestRule<>(LandingPage.class);
