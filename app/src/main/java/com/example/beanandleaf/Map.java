a30fc8b27330e765997b996c373c7980516f5be5
Stacy Tran <stacytra@usc.edu>
2019-12-07 15:18:32 -0800

    Current location changes

--- app/src/main/java/com/example/beanandleaf/Map.java
+++ app/src/main/java/com/example/beanandleaf/Map.java
@@ -5,28 +5,18 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.os.Looper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.location.Location;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -39,9 +29,6 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -70,6 +57,7 @@ public class Map extends Fragment implements OnMapReadyCallback {
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+//        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 //        startLocationUpdates();
 
 
@@ -94,7 +82,7 @@ public class Map extends Fragment implements OnMapReadyCallback {
 //        settingsClient.checkLocationSettings(locationSettingsRequest);
 //
 //        // new Google API SDK v11 uses getFusedLocationProviderClient(this)
-//        getFusedLocationProviderClient(this).requestLocationUpdates(mLocationRequest, new LocationCallback() {
+//        LocationServices.getFusedLocationProviderClient(this).requestLocationUpdates(mLocationRequest, new LocationCallback() {
 //                    @Override
 //                    public void onLocationResult(LocationResult locationResult) {
 //                        // do work here
@@ -104,19 +92,30 @@ public class Map extends Fragment implements OnMapReadyCallback {
 //                Looper.myLooper());
 //    }
 
-    public void onLocationChanged(Location location) {
-        // New location has now been determined
-        String msg = "Updated Location: " +
-                Double.toString(location.getLatitude()) + "," +
-                Double.toString(location.getLongitude());
-        Toast.makeText(this.getContext(), msg, Toast.LENGTH_SHORT).show();
-        // You can now create a LatLng Object for use with maps
-        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-    }
-
+//    public void onLocationChanged(Location location) {
+//        // New location has now been determined
+//        String msg = "Updated Location: " +
+//                Double.toString(location.getLatitude()) + "," +
+//                Double.toString(location.getLongitude());
+//        Toast.makeText(this.getContext(), msg, Toast.LENGTH_SHORT).show();
+//        // You can now create a LatLng Object for use with maps
+//        Float currentLat = (float)location.getLatitude();
+//        Float currentLong = (float)location.getLongitude();
+//        //Compare to stores long and lat nearby
+//        DatabaseHelper db = new DatabaseHelper(getActivity());
+//        ArrayList<Store> stores = db.getStores();
+//        for (Store s : stores) {
+//            if (s.isVerified()) {
+//                if (s.getLatitude() == currentLat && s.getLongitude() == currentLong){
+//                    //Ask user to log order
+//                }
+//            }
+//        }
+//    }
+//
 //    public void getLastLocation() {
 //        // Get last known recent location using new Google Play Services SDK (v11+)
-//        FusedLocationProviderClient locationClient = getFusedLocationProviderClient();
+//        FusedLocationProviderClient locationClient = LocationServices.getFusedLocationProviderClient();
 //
 //        locationClient.getLastLocation()
 //                .addOnSuccessListener(new OnSuccessListener<Location>() {
